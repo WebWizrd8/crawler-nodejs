99a6efcae8eaca9af389ea4c847b52171d821305
WebWizrd8 <superultradev000@gmail.com>
2023-09-10 11:05:52 -0400

    logger

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
 import { ENDPOINT_URL } from "./constants/env";
 import { WorkerMessage } from "./types/worker";
 import { worker } from "./worker/worker";
-
+import winston from "winston";
 import { Worker, isMainThread, workerData } from "worker_threads";
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "crawler-node" },
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+  ],
+});
 
 function initWorker(id: number) {
   const worker = new Worker("./build/worker/main.js", {
     workerData: {
       chainId: id,
       endpoint: ENDPOINT_URL,
+      trackLastNTransactions: 1000,
     },
   });
 
@@ -17,19 +29,20 @@ function initWorker(id: number) {
 
   // Receive messages from the worker thread
   worker.on("message", (message) => {
-    console.log(`Received from worker: ${message}`);
+    logger.debug(`Received from worker: ${message}`);
   });
 
   // Handle errors from the worker thread
+  // TODO: handle different errors here
   worker.on("error", (error) => {
-    console.error(`Worker error: ${error}`);
+    logger.error(`Worker error: ${error}`);
     initWorker(id);
   });
 
   // Handle the worker thread exiting
   worker.on("exit", (code) => {
     if (code !== 0) {
-      console.error(`Worker stopped with exit code ${code}`);
+      logger.error(`Worker stopped with exit code ${code}`);
       initWorker(id);
     }
   });
