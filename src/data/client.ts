4226fe228b32f81afdda5b86886506489b5280b0
WebWizrd8 <superultradev000@gmail.com>
2023-11-21 12:24:04 -0500

    fix datasources names

--- src/data/client.ts
+++ src/data/client.ts
@@ -3,25 +3,25 @@ import { DB_URL } from "../constants/env";
 import { IAlert, IChain, IChainEndpoint, IDestination } from "../types/types";
 
 export const getChains = async (): Promise<IChain[]> =>
-  (await fetch(DB_URL + "chains")).json();
+  (await fetch(DB_URL + "chain")).json();
 
 export const getChain = async (id: IChain["id"]): Promise<IChain> =>
-  (await fetch(DB_URL + "chains?id=eq." + id)).json();
+  (await fetch(DB_URL + "chain?id=eq." + id)).json();
 
 export const getChainEndpoints = async (
   chainId: IChain["id"],
 ): Promise<IChainEndpoint[]> =>
-  (await fetch(DB_URL + "chains_endpoints?chain_id=eq." + chainId)).json();
+  (await fetch(DB_URL + "chain_endpoint?chain_id=eq." + chainId)).json();
 
 export const getDestination = async (
   id: IDestination["id"],
 ): Promise<IDestination> =>
-  (await fetch(DB_URL + "destinations?id=eq." + id)).json();
+  (await fetch(DB_URL + "destination?id=eq." + id)).json();
 
 export const getAlert = async (
   id: IAlert["id"],
 ): Promise<IAlert> =>
-  (await fetch(DB_URL + "alerts?id=eq." + id)).json();
+  (await fetch(DB_URL + "alert?id=eq." + id)).json();
 
 export const getDestinationsForTransaction = async (event_data: Record<string, unknown>): Promise<IDestination[]> =>
   (await fetch(DB_URL + "rpc/get_destination_targets", {
